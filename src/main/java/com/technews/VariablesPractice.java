8594898b4f687653ae975a6e790997b48eb6aa63
Monica <monicadolcemascolo@gmail.com>
2023-01-04 14:12:26 -0500

    creating entity models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int number = 10;
+        double dec = 2.5;
+
+        double sum = number + dec;
+
+        String hello = "Hello" + number;
+        System.out.println(sum + hello);
+
+    }
+}
