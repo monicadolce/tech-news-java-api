0ca653c5c4d0d1d061348dde6e00e837f3c7e4c9
Monica <monicadolcemascolo@gmail.com>
2023-01-05 12:42:00 -0500

    fixed issues with post

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -5,6 +5,8 @@ import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -14,11 +16,14 @@ public class Post implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
+    private String title;
+    private String postUrl;
     @Transient
     private String userName;
     @Transient
     private int voteCount;
+
+    private Integer userId;
 
     @NotNull
     @Temporal(TemporalType.DATE)
@@ -33,10 +38,13 @@ public class Post implements Serializable {
     @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
 
-    public Post(Integer id, String userName, int voteCount, Date postedAt, Date updatedAt, List<Comment> comments) {
+    public Post(Integer id, String title, String postUrl, String userName, int voteCount, Integer userId, Date postedAt, Date updatedAt, List<Comment> comments) {
         this.id = id;
+        this.title = title;
+        this.postUrl = postUrl;
         this.userName = userName;
         this.voteCount = voteCount;
+        this.userId = userId;
         this.postedAt = postedAt;
         this.updatedAt = updatedAt;
         this.comments = comments;
@@ -48,6 +56,22 @@ public class Post implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPostUrl() {
+        return postUrl;
+    }
+
+    public void setPostUrl(String postUrl) {
+        this.postUrl = postUrl;
     }
 
     public String getUserName() {
@@ -64,6 +88,14 @@ public class Post implements Serializable {
 
     public void setVoteCount(int voteCount) {
         this.voteCount = voteCount;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
     public Date getPostedAt() {
@@ -95,27 +127,28 @@ public class Post implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return voteCount == post.voteCount && Objects.equals(id, post.id) && Objects.equals(userName, post.userName) && Objects.equals(postedAt, post.postedAt) && Objects.equals(updatedAt, post.updatedAt) && Objects.equals(comments, post.comments);
+        return voteCount == post.voteCount && Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(postUrl, post.postUrl) && Objects.equals(userName, post.userName) && Objects.equals(userId, post.userId) && Objects.equals(postedAt, post.postedAt) && Objects.equals(updatedAt, post.updatedAt) && Objects.equals(comments, post.comments);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, userName, voteCount, postedAt, updatedAt, comments);
+        return Objects.hash(id, title, postUrl, userName, voteCount, userId, postedAt, updatedAt, comments);
     }
 
     @Override
     public String toString() {
         return "Post{" +
                 "id=" + id +
+                ", title='" + title + '\'' +
+                ", postUrl='" + postUrl + '\'' +
                 ", userName='" + userName + '\'' +
                 ", voteCount=" + voteCount +
+                ", userId=" + userId +
                 ", postedAt=" + postedAt +
                 ", updatedAt=" + updatedAt +
                 ", comments=" + comments +
                 '}';
     }
-
-
 }
 
 
